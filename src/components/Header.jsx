eafe5fded20d2750765789fa7f28a39968855f4c
Hai <haiek55tc1@gmail.com>
2022-10-20 23:37:26 +0700

    i

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "./img/logo.png";
 const Header = () => {
   return (
-    <div className = "fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <div className="fixed z-50 w-screen p-6 px-16">
       {/* {desktop & tablet} */}
-      <div className = "hidden md:flex w-full">
-
+      <div className="hidden md:flex w-full h-full">
+        <div className="flex items-center gap-2">
+          <img src={Logo} classname="w-10 object-cover" alt="logo" />
+        </div>
       </div>
 
       {/* mobile */}
-      <div className="block md:hidden w-full h-full"></div>
+      <div className="flex md:hidden w-full h-full"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
