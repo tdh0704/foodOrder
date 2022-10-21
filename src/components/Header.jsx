5c6386aee34f0de8939585afb98b83ae9e5c0944
Hai <haiek55tc1@gmail.com>
2022-10-21 22:17:50 +0700

    update auth not fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Logo from "../img/logo.png";
 import {MdShoppingBasket} from "react-icons/md";
+
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {app} from "../firebase.config";
+
+
 import Avatar from '../img/avatar.png';
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const Header = () => {
+
+const firebaseAuth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+
+const [{user}, dispatch] = useStateValue();
+  const login = async () => {
+    const {user : {refreshToken,providerData}} = await signInWithPopup(firebaseAuth, provider);
+    dispatch({
+      type: actionType.SET_USER,
+      user : providerData[0],
+    })
+  }
+
+
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
       {/* {desktop & tablet} */}
@@ -38,8 +61,14 @@ const Header = () => {
           </div>
         </div>
 
+        <div className = "relative">
         <motion.img whileTap = {{scale: 0.6}}
-        src={Avatar} className = "w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" alt="userprofile" />
+        src={user ? user.photoURL : Avatar} 
+        className = "w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full" 
+        alt="userprofile" 
+        onClick = {login}
+        />
+        </div>
         </div>
       </div>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import './index.css'
+import { StateProvider } from "./context/StateProvider";
 
 
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App'
+import { initialState } from "./context/initalState";
+import reducer from "./context/reducer";
 
 ReactDOM.render(
     <Router>
-         <App />
+         <StateProvider inittialState = {initialState} reducer = {reducer}>
+             <App/>
+         </StateProvider>
     </Router>
    ,    
     document.getElementById("root")
